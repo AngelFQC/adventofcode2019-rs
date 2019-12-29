36ea5bf9bacd6a98e3f8218e26d89e6ae15276ba
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2019-12-29 05:31:56 -0500

    Day 3: Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -32,18 +32,19 @@ fn get_points(instructions: Vec<&str>) -> Vec<(i32, i32)> {
     points
 }
 
-fn compare_points(points_a: Vec<(i32, i32)>, points_b: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+fn get_crossing_points(points_a: &Vec<(i32, i32)>, points_b: &Vec<(i32, i32)>) -> Vec<(i32, i32)> {
     let mut comparison = Vec::new();
 
     for point_a in points_a {
         if points_b.contains(&point_a) {
-            comparison.push(point_a);
+            comparison.push(*point_a);
         };
     }
 
     comparison
 }
 
+#[allow(dead_code)]
 fn find_closest(comparison: Vec<(i32, i32)>) -> (i32, i32) {
     let mut closest = comparison[0];
     let mut distance = (closest.0).abs() + (closest.1).abs();
@@ -59,8 +60,44 @@ fn find_closest(comparison: Vec<(i32, i32)>) -> (i32, i32) {
     closest
 }
 
+#[allow(dead_code)]
 fn get_distance(closest: (i32, i32)) -> i32 {
     closest.0.abs() + closest.1.abs()
+}
+
+fn get_steps_to_reach_point(point: (i32, i32), wire_points: &Vec<(i32, i32)>) -> i32 {
+    let mut iter = wire_points.split(|p| p.0 == point.0 && p.1 == point.1);
+
+    let steps = match iter.next() {
+        Some(steps) => steps,
+        None => panic!("No steps in points"),
+    };
+
+    steps.len() as i32 + 1
+}
+
+fn get_best_steps(
+    comparison: Vec<(i32, i32)>,
+    wire_points_a: Vec<(i32, i32)>,
+    wire_points_b: Vec<(i32, i32)>,
+) -> i32 {
+    let mut totals = Vec::new();
+
+    for point in comparison {
+        let distance_a = get_steps_to_reach_point(point, &wire_points_a);
+        let distance_b = get_steps_to_reach_point(point, &wire_points_b);
+
+        totals.push(distance_a + distance_b);
+    }
+
+    totals.sort();
+
+    let better = match totals.get(0) {
+        Some(first) => first,
+        None => panic!("First better intersection not found."),
+    };
+
+    *better
 }
 
 fn main() {
@@ -82,13 +119,11 @@ fn main() {
     let points_a = get_points(get_instructions(line_a_str));
     let points_b = get_points(get_instructions(line_b_str));
 
-    let comparison = compare_points(points_a, points_b);
+    let comparison = get_crossing_points(&points_a, &points_b);
 
-    let closest = find_closest(comparison);
+    let better = get_best_steps(comparison, points_a, points_b);
 
-    let distance = get_distance(closest);
-
-    println!("Manhattan distance: {}", distance);
+    println!("Fewest combined steps is: {}", better);
 }
 
 #[cfg(test)]
--- day3/src/test.rs
+++ day3/src/test.rs
@@ -70,7 +70,7 @@ fn getting_points() {
 fn comparing_points() {
     let (points_a, points_b) = setup_points();
 
-    let comparison = compare_points(points_a, points_b);
+    let comparison = get_crossing_points(&points_a, &points_b);
 
     assert_eq!(vec![(6, 5), (3, 3)], comparison);
 }
@@ -78,7 +78,7 @@ fn comparing_points() {
 #[test]
 fn finding_closest() {
     let (points_a, points_b) = setup_points();
-    let comparison = compare_points(points_a, points_b);
+    let comparison = get_crossing_points(&points_a, &points_b);
     let closest = find_closest(comparison);
 
     assert_eq!((3, 3), closest);
@@ -87,9 +87,37 @@ fn finding_closest() {
 #[test]
 fn getting_distance() {
     let (points_a, points_b) = setup_points();
-    let comparison = compare_points(points_a, points_b);
+    let comparison = get_crossing_points(&points_a, &points_b);
     let closest = find_closest(comparison);
     let distance = get_distance(closest);
 
     assert_eq!(6, distance);
+}
+
+#[test]
+fn getting_best_steps() {
+    let data = [
+        vec![
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+            "U62,R66,U55,R34,D71,R55,D58,R83",
+            "610",
+        ],
+        vec![
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+            "410",
+        ],
+    ];
+
+    for d in data.iter() {
+        let points_a = get_points(get_instructions(d[0]));
+        let points_b = get_points(get_instructions(d[1]));
+
+        let comparison = get_crossing_points(&points_a, &points_b);
+        let better = get_best_steps(comparison, points_a, points_b);
+
+        let expected_better: i32 = d[2].parse().unwrap();
+
+        assert_eq!(expected_better, better);
+    }
 }
